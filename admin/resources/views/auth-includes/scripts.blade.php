378233f88297d3e834be762d7bc46ab6d3ae8133
Sebgatullah/ITP/Md. Sebgatullah Mahbub (Email: sebgatullah.mahbub@reddotdigitalit.com) <sebgatullah.mahbub@reddotdigitalit.com>
2023-05-27 23:41:52 +0600

    Remove admin folder from this branch

--- admin/resources/views/auth-includes/scripts.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- jQuery -->
-<script src="{{asset('asset/plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('asset/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('asset/js/adminlte.min.js')}}"></script>
